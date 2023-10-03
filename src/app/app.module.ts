70c2e439e42a592f7d8507d77dc9da02178d5f88
fender-kid <jtt.sides@gmail.com>
2023-10-02 20:26:02 -0500

    Added core components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FiltersComponent } from './filters/filters.component';
+import { MovieInfoComponent } from './movie-info/movie-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FiltersComponent,
+    MovieInfoComponent
   ],
   imports: [
     BrowserModule
