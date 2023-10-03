febf4f39b4f264d34a5f25ae9e65546522734295
fender-kid <jtt.sides@gmail.com>
2023-10-02 23:36:35 -0500

    Added a sortMoviesByStatus Method.

---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  id?: number;
+  title: string;
+  releaseDate: string;
+  status: string;
+  rating: string;
+  platform: string;
+}
--- src/app/movie-info/movie-info.component.ts
+++ src/app/movie-info/movie-info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Movie } from '../models/movie.model';
 
 @Component({
   selector: 'app-movie-info',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-info.component.css']
 })
 export class MovieInfoComponent {
+  movies: Movie[] = [
+    {
+      title: 'Movie A',
+      releaseDate: '2023-01-01',
+      status: 'Watched',
+      rating: 'PG-13',
+      platform: 'Netflix',
+    },
+    {
+      title: 'Movie B',
+      releaseDate: '2023-12-01',
+      status: 'Unwatched',
+      rating: 'R',
+      platform: 'Hulu',
+    },
+  ];
 
+  sortMoviesByStatus(): void {
+    this.movies.sort((a, b) => a.status.localeCompare(b.status));
+  }
 }
+
